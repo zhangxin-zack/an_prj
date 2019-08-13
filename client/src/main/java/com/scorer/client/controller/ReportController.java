2e026bb0975a8cf6514070b64559141054fe2b9c
zhangxin-zack <zhangxin.purvar.wh@gmail.com>
2019-08-13 10:51:15 +0800

    0813

--- client/src/main/java/com/scorer/client/controller/ReportController.java
+++ client/src/main/java/com/scorer/client/controller/ReportController.java
@@ -16,16 +16,31 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
+    /**
+     * 获取警报信息
+     * @param page
+     * @return
+     */
     @RequestMapping(value = "/alarm_list")
     public Map getAlarmReportList(@RequestBody PageBean page) {
         return reportService.getAlarmReportList(page);
     }
 
+    /**
+     * 获取日报表
+     * @param page
+     * @return
+     */
     @RequestMapping(value = "/actual_list")
     public Map getActualReportList(@RequestBody PageBean page) {
         return reportService.getActualReportList(page);
     }
 
+    /**
+     * 获取历史记录(当日/当月)
+     * @param page
+     * @return
+     */
     @RequestMapping(value = "/history_list")
     public Map getHistoryReportList(@RequestBody PageBean page) {
         return reportService.getHistoryReportList(page);
