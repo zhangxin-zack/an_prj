a7188bb996bf815ece20451c7642ebebdf1e12fd
AnYimin <145062@qq.com>
2019-09-04 00:53:31 +0800

    1

--- feign/src/main/java/com/scorer/feign/_WebSocket/WebSocketConfig.java
+++ feign/src/main/java/com/scorer/feign/_WebSocket/WebSocketConfig.java
@@ -1,7 +1,7 @@
 package com.scorer.feign._WebSocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -10,7 +10,7 @@ import javax.annotation.Resource;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+public class WebSocketConfig extends WebMvcConfigurationSupport implements WebSocketConfigurer {
 
     @Resource
     private WebSocketPushHandler handler;
